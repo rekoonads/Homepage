badee761f677061baac43ad3653b094db83a85b3
Ayan Pakhira <pakhiraayan@gmail.com>
2024-07-22 19:12:01 +0530

    fixing bugs

--- src/main.tsx
+++ src/main.tsx
@@ -8,18 +8,10 @@ import 'jsvectormap/dist/css/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
 import { ClerkProvider } from '@clerk/clerk-react';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key');
-}
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <Router>
-        <App />
-      </Router>
-    </ClerkProvider>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
