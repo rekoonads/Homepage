0c0a6113b2a61cedb220569790cde0e3bbda6fb7
Ayan Pakhira <pakhiraayan@gmail.com>
2024-07-22 19:19:39 +0530

    fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 
 import Loader from './common/Loader';
 
-import { useUser } from '@clerk/clerk-react';
 import Home from './pages/Home';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const { pathname } = useLocation();
-  const { user, isLoaded, isSignedIn } = useUser();
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -18,12 +16,6 @@ function App() {
   useEffect(() => {
     setTimeout(() => setLoading(false), 1000);
   }, []);
-
-  useEffect(() => {
-    if (isLoaded && isSignedIn && pathname === '/') {
-      window.location.replace('/dashboard');
-    }
-  }, [isLoaded, isSignedIn, pathname]);
 
   if (loading) {
     return <Loader />;
